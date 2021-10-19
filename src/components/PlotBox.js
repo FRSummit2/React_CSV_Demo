51f91dc8ce9ca4b02fa828399210d50726e7863c
DESKTOP-OFS5H6F\User <frsummit1@gmail.com>
2021-10-19 17:35:30 +0600

    plot works properly

--- src/components/PlotBox.js
+++ src/components/PlotBox.js
@@ -1,21 +1,60 @@
 import React from 'react'
-import Histogram from 'react-chart-histogram';
+import { Boxplot, computeBoxplotStats } from 'react-boxplot'
 
 class Plot extends React.Component {
     render() {
-        const labels = ['2016', '2017', '2018'];
-        const data = [324, 45, 672];
-        const options = { fillColor: '#FFFFFF', strokeColor: '#0000FF' };
+        const values = [
+            14,
+            15,
+            16,
+            16,
+            17,
+            17,
+            17,
+            17,
+            17,
+            18,
+            18,
+            18,
+            18,
+            18,
+            18,
+            19,
+            19,
+            19,
+            20,
+            20,
+            20,
+            20,
+            20,
+            20,
+            21,
+            21,
+            22,
+            23,
+            24,
+            24,
+            29,
+        ]
+
         return (
-            <div>
-                <Histogram
-                    xLabels={labels}
-                    yValues={data}
-                    width='400'
-                    height='200'
-                    options={options}
+            <>
+                <Boxplot
+                    width={400}
+                    height={25}
+                    orientation="horizontal"
+                    min={0}
+                    max={300}
+                    stats={{
+                        whiskerLow: 194.3,
+                        quartile1: 201,
+                        quartile2: 234.5,
+                        quartile3: 254.6,
+                        whiskerHigh: 257.95,
+                        outliers: [50, 75, 184.25, 268, 290],
+                    }}
                 />
-            </div>
+            </>
         )
     }
 }
--- src/components/PlotScatter.js
+++ src/components/PlotScatter.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid } from 'recharts';
-  
+import Chart from "react-google-charts";
+
 const ScatterPlot = () => {
-  
+
     // Sample data
     const data = [
         { x: 1, y: 23 },
@@ -15,15 +16,40 @@ const ScatterPlot = () => {
         { x: 8, y: 32 },
         { x: 9, y: 43 },
     ];
-  
+
     return (
-        <ScatterChart width={400} height={400}>
-            <CartesianGrid />
-            <XAxis type="number" dataKey="x" />
-            <YAxis type="number" dataKey="y" />
-            <Scatter data={data} fill="green" />
-        </ScatterChart>
+        <>
+            <ScatterChart width={400} height={400}>
+                <CartesianGrid />
+                <XAxis type="number" dataKey="x" />
+                <YAxis type="number" dataKey="y" />
+                <Scatter data={data} fill="green" />
+            </ScatterChart>
+            
+            <Chart
+                width={'600px'}
+                height={'400px'}
+                chartType="ScatterChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    ['Age', 'Weight'],
+                    [8, 12],
+                    [4, 5.5],
+                    [11, 14],
+                    [4, 5],
+                    [3, 3.5],
+                    [6.5, 7],
+                ]}
+                options={{
+                    title: 'Age vs. Weight comparison',
+                    hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
+                    vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
+                    legend: 'none',
+                }}
+                rootProps={{ 'data-testid': '1' }}
+            />
+        </>
     );
 }
-  
+
 export default ScatterPlot;
