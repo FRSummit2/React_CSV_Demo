d7e29c42d70df15f131380749b480142db2918bf
DESKTOP-OFS5H6F\User <frsummit1@gmail.com>
2021-10-19 11:42:42 +0600

    table rander properly

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,41 +1,50 @@
-import React from 'react';
-import * as XLSX from 'xlsx';
+import React from 'react'
+import * as XLSX from 'xlsx'
 import { useDispatch } from 'react-redux'
-import { csvTransection } from '../actions';
+import { csvTransection } from '../actions'
 import { useHistory } from "react-router"
 
 const Page1 = () => {
 
     const dispatch = useDispatch()
-    const history = useHistory();
+    const history = useHistory()
 
     const processData = dataString => {
-        const dataStringLines = dataString.split(/\r\n|\n/);
-        const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+        const dataStringLines = dataString.split(/\r\n|\n/)
+        const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/)
 
-        const list = [];
+        const list = []
+        const list_table = []
         for (let i = 1; i < dataStringLines.length; i++) {
-            const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+            const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/)
             if (headers && row.length === headers.length) {
-                const obj = {};
+                const obj = {}
+                const obj_table = []
                 for (let j = 0; j < headers.length; j++) {
                     let d = row[j];
                     if (d.length > 0) {
                         if (d[0] === '"')
-                            d = d.substring(1, d.length - 1);
+                            d = d.substring(1, d.length - 1)
                         if (d[d.length - 1] === '"')
-                            d = d.substring(d.length - 2, 1);
+                            d = d.substring(d.length - 2, 1)
                     }
+                    // console.log(d)
+                    // console.log(headers[j])
                     if (headers[j]) {
-                        obj[headers[j]] = d;
+                        obj[headers[j]] = d
+                        obj_table.push(d)
                     }
                 }
 
                 if (Object.values(obj).filter(x => x).length > 0) {
-                    list.push(obj);
+                    // console.log(obj)
+                    list.push(obj)
+                    list_table.push(obj_table)
                 }
             }
         }
+        // console.log(list)
+        // console.log(list_table)
 
         const columns = headers.map(c => ({
             name: c,
@@ -44,28 +53,29 @@ const Page1 = () => {
 
         const tableData = {
             header: columns,
-            body: list
+            body: list,
+            table_data: list_table
         }
         console.log(tableData)
         dispatch(csvTransection(tableData))
-        
+
         history.push({
             pathname:  "/page2"
          });
     }
 
     const handleFileUpload = e => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
+        const file = e.target.files[0]
+        const reader = new FileReader()
         reader.onload = (evt) => {
-            const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, { type: 'binary' });
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-            processData(data);
+            const bstr = evt.target.result
+            const wb = XLSX.read(bstr, { type: 'binary' })
+            const wsname = wb.SheetNames[0]
+            const ws = wb.Sheets[wsname]
+            const data = XLSX.utils.sheet_to_csv(ws, { header: 1 })
+            processData(data)
         };
-        reader.readAsBinaryString(file);
+        reader.readAsBinaryString(file)
     }
 
     return (
--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,77 +1,72 @@
-import React from 'react';
+import React from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from "react-router"
 
 const Page2 = () => {
+
+    const history = useHistory();
 
     const csvData = useSelector(state => state.csvTransection)
     console.log(csvData)
 
+    const returnUploadComponent = () => {
+        setTimeout( () => {
+            history.push({
+                pathname: "/"
+            });
+        }, 1500)
+    }
+
     return (
         <div className="container-fluid">
-            <h4>CSV Data Table</h4>
-            <div className="table-responsive">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                            <th scope="col">Heading</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            {
+                csvData ?
+                    <>
+                        <h4>CSV Data Table</h4>
+                        <div className="table-responsive">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        {
+                                            csvData.header.map((element, index) => (
+                                                <th scope="col" key={index}>{element.name}</th>
+                                            ))
+                                        }
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        csvData.table_data.map((element, index) => (
+                                            <tr key={index}>
+                                                {
+                                                    element.map((el, i) => (
+                                                        <td key={i}>{el}</td>
+                                                    ))
+                                                }
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" className="btn btn-secondary">Download CSV File</button>
+                        </div>
+
+                        <div className="grid-container mt-4">
+                            <div className="btn-group" role="group" aria-label="Basic example">
+                                <button type="button" className="btn btn-secondary">Download PDF</button>
+                            </div>
+                        </div>
+                    </> :
+                    <>
+                        <h3>No CSV found</h3>
                         {
-                            Array.from({ length: 10 }, (_, index) => (
-                                <tr key={index}>
-                                    <th scope="row">{index + 1}</th>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                    <td>Cell</td>
-                                </tr>
-                            ))
+                            returnUploadComponent()
                         }
-                    </tbody>
-                </table>
-            </div>
-            <div className="btn-group" role="group" aria-label="Basic example">
-                <button type="button" className="btn btn-secondary">Download CSV File</button>
-            </div>
-            
-            <div className="grid-container mt-4">
-                <div className="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" className="btn btn-secondary">Download PDF</button>
-                </div>
-            </div>
+                    </>
+            }
+
         </div>
     );
 }
